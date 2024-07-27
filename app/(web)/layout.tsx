d26d9ad25fa1e36f21ef5be0f56be8e1a23c0dfc
somadina6 <somadina6@gmail.com>
2024-07-27 03:25:17 -0300

    fix overflow on mobile

--- app/(web)/layout.tsx
+++ app/(web)/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className}`}>
         <ThemeProvider>
-          <main className="font-normal px-12 py-4 min-h-lvh">
+          <main className="font-normal px-12 py-4 min-h-lvh overflow-x-hidden">
             <Navbar />
             {children}
           </main>
