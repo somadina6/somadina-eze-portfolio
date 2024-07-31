4687920ada0a4d01a97e395d36a3412727474e8d
Somadina Eze <140349370+somadina6@users.noreply.github.com>
2024-07-31 16:09:35 -0300

    Update AboutMe.tsx

--- components/AboutMe/AboutMe.tsx
+++ components/AboutMe/AboutMe.tsx
@@ -3,7 +3,7 @@
 const AboutMe = () => {
   return (
     <section className="divbg col-span-12 md:col-span-6 row-span-2 md:row-span-2 gap-1 md:gap-2 flex flex-col ">
-      <h1 className="myh2 uppercase font-bold">About Me</h1>
+      <h1 className="myh2 uppercase font-bold">About</h1>
       <p className="text-sm md:text-base tracking-wider ">
         Hello! I&apos;m Somadina Eze, a passionate and versatile Full Stack
         Developer based in Saint John, NB. With a strong background in both
