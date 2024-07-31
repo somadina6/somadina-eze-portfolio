0b767bf2070a6482b131b6bd61ef18e846722a7f
Somadina Eze <140349370+somadina6@users.noreply.github.com>
2024-07-31 16:06:09 -0300

    Update MyStack.tsx

--- components/MyStack/MyStack.tsx
+++ components/MyStack/MyStack.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const MyStack = () => {
   return (
     <section className="divbg col-span-12 md:col-span-6 row-span-2 md:row-span-4 gap-1 md:gap-2 flex flex-col ">
-      <h2 className="myh2">My Stack</h2>
+      <h2 className="myh2 uppercase font-bold">My Stack</h2>
       <ul className="w-full flex gap-4 md:gap-6">
         {myStack.map(({ name, icon }) => (
           <li key={name} className="flex flex-col items-center">
