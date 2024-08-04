8aff659ba64eb790fbaf5f98d2f84bf804b5e106
somadina6 <somadina6@gmail.com>
2024-08-04 19:53:19 -0300

    g\refactor project card

--- components/Projects/ProjectsGrid.tsx
+++ components/Projects/ProjectsGrid.tsx
@@ -6,7 +6,7 @@ const ProjectsGrid = async () => {
   const myProjectsData = await getProjectsData();
   if (!myProjectsData) return null;
   return (
-    <section className="projects-grid grid-cols-1 lg:grid-cols-2">
+    <section className="projects-grid grid-cols-1 lg:grid-cols-2 xl:gap-5">
       {myProjectsData.map(
         ({
           _id,
