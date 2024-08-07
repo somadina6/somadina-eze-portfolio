2f04fcef6da9ac33781dd78d2de052423836139c
somadina6 <somadina6@gmail.com>
2024-08-06 23:57:30 -0300

    refactor project card component

--- components/Projects/ProjectsPage.tsx
+++ components/Projects/ProjectsPage.tsx
@@ -10,9 +10,9 @@ const ProjectsPage = async () => {
   return (
     <section className="w-full">
       <h1 className="w-full text-center myh1 mb-2 flex items-center pt-3 md:pt-6 pb-2 md:pb-4">
-        <hr className="flex-grow border-t-2 border-black-100" />
+        <hr className="flex-grow border-t-2 border-white rounded-full " />
         <span className="px-4">Here are some of my projects</span>
-        <hr className="flex-grow border-t-2 border-black-100" />
+        <hr className="flex-grow border-t-2 border-white rounded-full" />
       </h1>
       <ProjectsGrid projects={myProjectsData} />
     </section>
