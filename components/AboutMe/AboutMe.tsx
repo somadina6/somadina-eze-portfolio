cf5ad3ee7b34f2ff9ed00abbcceeb91bf7f13b0b
Somadina Eze <140349370+somadina6@users.noreply.github.com>
2025-05-17 21:25:29 -0300

    Update AboutMe.tsx

--- components/AboutMe/AboutMe.tsx
+++ components/AboutMe/AboutMe.tsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
       <p className="text-sm md:text-base tracking-wider text-center font-[300]">
         I&apos;m a{" "}
         <span className="font-semibold ">full-stack software engineer</span>{" "}
-        with over <span className="font-semibold ">3 years of experience.</span>
+        with over <span className="font-semibold ">5 years of experience.</span>
       </p>
       <p className="text-sm md:text-base tracking-wider text-center font-[300]">
         With a strong background in both front-end and back-end technologies, I
