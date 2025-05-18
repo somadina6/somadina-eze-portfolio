2c6d4e8a45ac4c6dd9f84a9978980186d563a961
Somadina Eze <somadina6@gmail.com>
2025-05-18 00:31:04 +0000

    2025 year

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="divbg flex flex-row items-center justify-between mt-2 md:mt-3">
       <p className="text-sm md:text-base">
-        © 2024 Somadina Eze. All rights reserved.
+        © 2025 Somadina Eze. All rights reserved.
       </p>
     </footer>
   );
